2da9a49f79d7b04fd1e6fae64f66818df52bdb11
LucileSD <4316@holbertonschool.com>
2022-08-17 13:33:25 +0000

    completion

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -12,7 +12,7 @@ file { 'html':
 file_line { 'redirection':
   ensure => present;
   path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server',
+  after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
