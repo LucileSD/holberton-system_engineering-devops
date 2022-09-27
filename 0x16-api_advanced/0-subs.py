2c28e40ca389e491254662b789ecb7887db653c3
LucileSD <4316@holbertonschool.com>
2022-09-27 09:05:53 +0000

    pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,15 @@
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
         querie the Reddit API and returns the number of subscribers for a given
         subreddit
     """
-    response = requests.get("https://www.reddit.com/r/" + subreddit + "/about.json", headers = {'User-agent': 'your bot 0.1'})
+    response = requests.get("https://www.reddit.com/r/" + subreddit +
+                            "/about.json",
+                            headers={'User-agent': 'your bot 0.1'})
     if response.status_code == 404:
         return 0
     return response.json()["data"]["subscribers"]
-
